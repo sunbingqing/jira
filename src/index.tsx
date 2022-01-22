2be5877794ff77a7b62d31d2d965829d02eb4d76
sunbingqing <sunbingqing@xiaomai5.com>
2022-01-22 16:23:25 +0800

    fix init

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+ * @Author: sunbingqing
+ * @Date: 2022-01-22 13:59:34
+ * @LastEditors: sunbingqing
+ * @LastEditTime: 2022-01-22 16:22:44
+ * @Description:
+ * @Copyrigh: © 2020 杭州杰竞科技有限公司 版权所有
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
